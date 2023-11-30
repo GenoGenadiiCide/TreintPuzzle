a9ba4bbc53d7b9532b3125b98271a7ffca5c6567
GenoGenadiiCide <amirkaokami@gmail.com>
2023-11-30 16:52:17 +0600

    upd0035

--- android/app/src/main/kotlin/com/example/treintpuzzle/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/treintpuzzle/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.treintpuzzle
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import android.view.View
-import android.view.WindowManager
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
 
-        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                                                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                                                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
     }
 }
